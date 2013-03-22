cfb959d865cfa2a522b70606405d9728184491be
kbokhonko <mykola.bokhonko@perfectial.com>
2013-03-22 14:11:52 +0200

    initial project version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Battlesea::Application.config.secret_token = '703552e1e8c46ac60ac141dab25a5b0b6120607f2f518a8f1d84905c52bd80071cabff1df2150810bf26f7134db88e23e04eea2926309b23512680bc664743e6'
